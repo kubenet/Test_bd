b674fdc1d60ce7f040568ef1fd1c6c9c237e4f2a
kubenet <kubenet@gmail.com>
2020-07-27 19:49:21 +0700

    first commit

--- main.py
+++ main.py
@@ -33,25 +33,25 @@ class User(Base):
 
 
 # Создание таблицы
-Base.metadata.drop_all(engine)
+#Base.metadata.drop_all(engine)
 Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
 Session.configure(bind=engine)  # Как только у вас появится engine
 session = Session()
 
-vasia_user1 = User("Igor", "k.", "4444")
-vasia_user2 = User("Dima", "V.", "88888333")
-vasia_user3 = User("Serega", "A.", "998667")
+vasia_user1 = User("Sergey", "S.", "8888")
 
-session.add_all( [vasia_user1, vasia_user3, vasia_user2] )   #добавить сразу пачку записей
+#session.add_all( [vasia_user1] )   #добавить сразу пачку записей
+#session.commit()
+#print(f" Имя нового пользователя {vasia_user1.name} и id: {vasia_user1.id}")
 
-session.commit()
-
-print(f" Имя нового пользователя {vasia_user1.name} и id: {vasia_user1.id}")
-
-for instance in session.query(User).filter(User.name=='Dima'):
-    instance.name='Oleg'
-    session.add_all([instance])  # добавить сразу пачку записей
-    session.commit()
-    print(instance.name, instance.fullname, instance.password)
+for instance in session.query(User).filter(User.id):
+    if instance.name==vasia_user1.name:
+        print('Такая запись уже есть')
+    else:
+        #instance.name='Oleg'
+        session.add_all([vasia_user1])  # добавить сразу пачку записей
+        session.commit()
+        print(instance.name, instance.fullname, instance.password)
+print(instance)
