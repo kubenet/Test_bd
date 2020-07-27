86f65f8455d2d85ca2cd4d990494933c8a72e3e0
kubenet <kubenet@gmail.com>
2020-07-27 20:09:55 +0700

    first commit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+import time
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -19,39 +21,44 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-    fullname = Column(String)
-    password = Column(String)
+    sensor_id = Column(String)
+    temperature = Column(Integer)
 
-    def __init__(self, name, fullname, password):
-        self.name = name
-        self.fullname = fullname
-        self.password = password
+    def __init__(self, sensor, temperature):
+        self.sensor_id = sensor
+        self.temperature = temperature
 
     def __repr__(self):
-        return "<User('%s','%s', '%s')>" % (self.name, self.fullname, self.password)
+        return "<User('%s','%d')>" % (self.sensor_id, self.temperature)
 
 
 # Создание таблицы
-#Base.metadata.drop_all(engine)
+Base.metadata.drop_all(engine)
 Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
 Session.configure(bind=engine)  # Как только у вас появится engine
 session = Session()
 
-vasia_user1 = User("Sergey", "S.", "8888")
+for i in range(100):
+    my_senor = User('Sens' + str(i), random.randint(25, 150))
+    session.add_all([my_senor])  # добавить сразу пачку записей
 
-#session.add_all( [vasia_user1] )   #добавить сразу пачку записей
-#session.commit()
-#print(f" Имя нового пользователя {vasia_user1.name} и id: {vasia_user1.id}")
+session.commit()
+
+# print(f" Имя нового пользователя {vasia_user1.name} и id: {vasia_user1.id}")
+
+# for instance in session.query(User).filter(User.id):
+#     if instance.name==vasia_user1.name:
+#         print('Такая запись уже есть')
+#     else:
+#         #instance.name='Oleg'
+#         session.add_all([vasia_user1])  # добавить сразу пачку записей
+#         session.commit()
+#         print(instance.name, instance.fullname, instance.password)
+# print(instance)
+
 
 for instance in session.query(User).filter(User.id):
-    if instance.name==vasia_user1.name:
-        print('Такая запись уже есть')
-    else:
-        #instance.name='Oleg'
-        session.add_all([vasia_user1])  # добавить сразу пачку записей
-        session.commit()
-        print(instance.name, instance.fullname, instance.password)
-print(instance)
+    print(instance.sensor_id, instance.temperature)
+    time.sleep(0.5)
